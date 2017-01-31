93ff19034e4e06c85fafe1c5a85bbe840316be51
Raffaele Mancino <raffaele.linux@gmail.com>
2017-01-31 13:15:13 +0100

    init ancient mod

---
+++ main/java/com/raffaele/ancient/Ancient.java
@@ -0,0 +1,15 @@
+package com.raffaele.ancient;
+
+import net.minecraft.init.Blocks;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = Info.ID, name = Info.NAME, version = Info.VERSION, acceptedMinecraftVersions = Info.MINECRAFT)
+public class Ancient
+{
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+    }
+}
---
+++ main/java/com/raffaele/ancient/Info.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.raffaele.ancient;
+
+/**
+ *
+ * @author Raffaele Francesco Mancino
+ */
+public class Info {
+    public static final String ID = "ancient";
+    public static final String NAME = "Ancient";
+    public static final String VERSION = "0.0";
+    public static final String MINECRAFT = "1.7.10";
+}
